e0163968394f509c3222aac92a6344b2b1a8ecb2
Vamshideep <vamshideep@gmail.com>
2014-09-21 15:17:23 -0700

    Hopefully Final Changes

--- ui.R
+++ ui.R
@@ -5,6 +5,7 @@ shinyUI(pageWithSidebar(
     h2('Enter your car data here'),
     selectInput('id1','Number of cylinders in your car',selected = "cyl",choices=list("4","6","8")),
     selectInput("id2", "is your car Automatic(0) or Manual(1)?",selected="am",choices=list("0","1")),
+    p(em("Documentation:",a("PredictMPG",href="index.html"))),
     submitButton('Submit')
     #numericInput('id3','Horse power of your car',0,min=2,max=10,step=2)
   ),
